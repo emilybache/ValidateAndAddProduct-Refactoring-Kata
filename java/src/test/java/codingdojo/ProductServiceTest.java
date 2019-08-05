487a0edaa6cf83e088e35aa515f7fe0a53dc4633
Emily Bache <emily.bache@praqma.com>
2019-08-05 11:11:10 +0200

    first test in place

--- java/src/test/java/codingdojo/ProductServiceTest.java
+++ java/src/test/java/codingdojo/ProductServiceTest.java
@@ -2,12 +2,35 @@ package codingdojo;
 
 import org.junit.jupiter.api.Test;
 
+import static org.approvaltests.Approvals.verify;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProductServiceTest {
     @Test
-    void sample() {
-        assertEquals(true, false);
+    void validateAndAdd() {
+        String name = "Sample product";
+        String type = "Lipstick";
+        double weight = 5;
+        double price = 10;
+        boolean recyclable = false;
+
+        ProductFormData productData = new ProductFormData(name, type, weight, price, recyclable);
+        FakeDatabase db = new FakeDatabase();
+        ProductService sut = new ProductService(db);
+        Response response = sut.validateAndAdd(productData);
+        String toVerify = "Response(" + response.getProductId() + ", " + response.getStatusCode() + ", " + response.getErrorMessage() + ")";
+
+        verify(toVerify);
+    }
+
+    class FakeDatabase extends DatabaseAccess {
+        private Product product;
+
+        @Override
+        public int storeProduct(Product product) {
+            this.product = product;
+            return 1;
+        }
     }
 
 }
