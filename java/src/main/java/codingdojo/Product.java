a235b24d87b5f041635aaebe2025c5b02a5ce164
Emily Bache <emily.bache@praqma.com>
2019-08-05 11:16:05 +0200

    better first test

--- java/src/main/java/codingdojo/Product.java
+++ java/src/main/java/codingdojo/Product.java
@@ -18,7 +18,7 @@ public class Product {
     }
 
     public Product(String name) {
-
+        this.name = name;
     }
 
     public String getName() {
--- java/src/test/java/codingdojo/ProductServiceTest.java
+++ java/src/test/java/codingdojo/ProductServiceTest.java
@@ -14,13 +14,29 @@ public class ProductServiceTest {
         double price = 10;
         boolean recyclable = false;
 
+        String toVerify = doValidateAndAdd(name, type, weight, price, recyclable);
+
+        verify(toVerify);
+    }
+
+    private String doValidateAndAdd(String name, String type, double weight, double price, boolean recyclable) {
         ProductFormData productData = new ProductFormData(name, type, weight, price, recyclable);
         FakeDatabase db = new FakeDatabase();
         ProductService sut = new ProductService(db);
         Response response = sut.validateAndAdd(productData);
-        String toVerify = "Response(" + response.getProductId() + ", " + response.getStatusCode() + ", " + response.getErrorMessage() + ")";
-
-        verify(toVerify);
+        String responseString = "Response("
+                + response.getProductId() + ", "
+                + response.getStatusCode() + ", "
+                + response.getErrorMessage()
+                + ")";
+        String productString = "Product("
+                + db.product.getName() + ", "
+                + db.product.getType() + ", "
+                + db.product.getWeight() + ", "
+                + db.product.getFamily() + ", "
+                + db.product.getRange()
+                + ")";
+        return responseString + " " + productString;
     }
 
     class FakeDatabase extends DatabaseAccess {
