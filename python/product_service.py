d7ba404f5982be1db1fd024c3981ec7f06fda316
Emily Bache <emily.bache@praqma.com>
2019-08-05 16:52:45 +0200

    fix bug with product range not being populated

--- python/product_service.py
+++ python/product_service.py
@@ -17,33 +17,33 @@ def validate_and_add(product_data: ProductFormData, db):
         if "Eyeshadow" == product_data.product_type and product.name.contains("Queen"):
             product.product_range = ProductRange.QUEEN
 
-    product.range = ProductRange.BUDGET
+    product.product_range = ProductRange.BUDGET
     if product_data.packaging_recyclable:
-        product.range = ProductRange.PROFESSIONAL
+        product.product_range = ProductRange.PROFESSIONAL
 
     if "Foundation" == product_data.product_type:
         if product_data.suggested_price > 10:
-            product.range = ProductRange.PROFESSIONAL
+            product.product_range = ProductRange.PROFESSIONAL
 
     if "Lipstick" == product_data.product_type:
         product.product_type = product_data.product_type
         product.product_family = ProductFamily.LIPS
 
     if product_data.suggested_price > 10:
-        product.range = ProductRange.PROFESSIONAL
+        product.product_range = ProductRange.PROFESSIONAL
 
     if product_data.suggested_price > 20:
         if 0 < product_data.weight < 10:
             return Response(0, -1, "Error - failed quality check for Queen Range")
-        product.range = ProductRange.QUEEN
+        product.product_range = ProductRange.QUEEN
 
     if "Mascara" == product_data.product_type:
         product.product_family = ProductFamily.LASHES
         if product_data.suggested_price > 15:
-            product.range = ProductRange.PROFESSIONAL
+            product.product_range = ProductRange.PROFESSIONAL
 
         if product_data.suggested_price > 25 and product_data.packaging_recyclable:
-            product.range = ProductRange.QUEEN
+            product.product_range = ProductRange.QUEEN
 
     if product_data.weight < 0:
         return Response(0, -3, "Weight error")
@@ -56,7 +56,7 @@ def validate_and_add(product_data: ProductFormData, db):
         if "Blusher" == product_data.product_type and product_data.weight > 10:
             return Response(0, -3, "Error - weight too high")
 
-    if not product_data.packaging_recyclable and product.range == ProductRange.QUEEN:
+    if not product_data.packaging_recyclable and product.product_range == ProductRange.QUEEN:
         return Response(0, -1, "Error - failed quality check for Queen Range")
 
     if "Unknown" == product.product_type:
