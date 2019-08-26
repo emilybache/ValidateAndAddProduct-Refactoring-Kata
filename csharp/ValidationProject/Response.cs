53106250971a759c600a99a2f19421192755de84
Emily Bache <emily@bacheconsulting.com>
2019-08-26 21:38:36 +0200

    csharp translation

---
+++ csharp/ValidationProject/Response.cs
@@ -0,0 +1,25 @@
+﻿namespace Validation
+{
+    public class Response
+    {
+        public int ProductId { get; }
+        public int StatusCode { get; }
+        public string Message { get; }
+
+        public Response(int productId, int statusCode, string message)
+        {
+            ProductId = productId;
+            StatusCode = statusCode;
+            Message = message;
+        }
+
+        public override string ToString()
+        {
+            return "Response{" +
+                   "productId=" + ProductId +
+                   ", statusCode=" + StatusCode +
+                   ", message='" + Message + '\'' +
+                   '}';
+        }
+    }
+}
---
+++ csharp/ValidationProject/ValidationTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+
+namespace Validation
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class ValidationTest
+    {
+        [Fact]
+        void ValidateAndAdd() {
+            // Arrange
+            var productData = new ProductFormData("Sample product",
+                "Lipstick", 5D, 10D, false);
+            var db = new FakeDatabase();
+            var sut = new ProductService(db);
+
+            // Act
+            var response = sut.ValidateAndAdd(productData);
+
+            // Assert
+            var productString = "";
+            if (db.Product != null) {
+                productString = db.Product.ToString();
+            }
+            var responseAndProduct = response + " " + productString;
+            Approvals.Verify(responseAndProduct);
+
+        }
+
+    }
+}
