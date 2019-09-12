938ce539b30dc35cc9d1d98346cb7c14f3183c29
Emily Bache <emily.bache@praqma.com>
2019-09-12 14:08:10 +0200

    added test using combination approvals

--- cpp/tests/product_service_test.cpp
+++ cpp/tests/product_service_test.cpp
@@ -3,19 +3,38 @@
 #include "doctest.h"
 #include "ApprovalTests.hpp"
 #include "FakeDatabase.h"
+#include <string>
 
-TEST_CASE("test_example_statement")
-{
-auto productData = new ProductFormData("Sample product", "Lipstick", 5, 10, false);
-auto db = new FakeDatabase();
-auto sut = new ProductService(db);
 
-Response response = sut->validateAndAdd(productData);
+string doValidateAndAdd(const string &name, const string &type, float weight, float price, bool packagingRecyclable);
 
-std::string responseAndProduct;
-responseAndProduct.append(response.to_string());
-responseAndProduct.append(" ");
-responseAndProduct.append(db->product->to_string());
+TEST_CASE ("test_example_statement") {
+    std::vector<std::string> names{"", "Sample product", "Sample Queen Product"};
+    std::vector<std::string> types{"", "Lipstick", "Eyeshadow", "Mascara", "Blusher", "Foundation", "Unknown"};
+    std::vector<float> weights{-1, 5, 11};
+    std::vector<float> prices{10, 11, 21, 26};
+    std::vector<bool> recyclables{false, true};
 
-Approvals::verify(responseAndProduct);
+    CombinationApprovals::verifyAllCombinations(doValidateAndAdd,
+                                                names,
+                                                types,
+                                                weights,
+                                                prices,
+                                                recyclables);
+}
+
+string doValidateAndAdd(const string &name, const string &type, float weight, float price, bool packagingRecyclable) {
+    auto productData = new ProductFormData(name, type, weight, price, packagingRecyclable);
+    auto db = new FakeDatabase();
+    auto sut = new ProductService(db);
+
+    Response response = sut->validateAndAdd(productData);
+
+    std::string responseAndProduct;
+    responseAndProduct.append(response.to_string());
+    if (db->product) {
+        responseAndProduct.append(" ");
+        responseAndProduct.append(db->product->to_string());
+    }
+    return responseAndProduct;
 }
