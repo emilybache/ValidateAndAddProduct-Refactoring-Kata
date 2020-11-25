1b2067d23df2ce4b64ce27b1f18eb189c721e453
emily <emily@bacheconsulting.com>
2020-11-25 13:31:30 +0100

    created function for combination approvals

--- java/src/test/java/codingdojo/ProductServiceTest.java
+++ java/src/test/java/codingdojo/ProductServiceTest.java
@@ -9,22 +9,33 @@ public class ProductServiceTest {
     @Test
     void validateAndAdd() {
         // Arrange
-        ProductFormData productData = new ProductFormData("Sample product",
-                "Lipstick", 5D, 10D, false);
+        String name = "Sample product";
+        String productType = "Lipstick";
+        double weight = 5D;
+        double suggestedPrice = 10D;
+        boolean packagingRecyclable = false;
+
+        // Act
+        String responseAndProduct = doValidateAndAdd(name, productType, weight, suggestedPrice, packagingRecyclable);
+
+        // Assert
+        verify(responseAndProduct);
+    }
+
+    private String doValidateAndAdd(String name, String productType, double weight, double suggestedPrice, boolean packagingRecyclable) {
+        ProductFormData productData = new ProductFormData(name,
+                productType, weight, suggestedPrice, packagingRecyclable);
         FakeDatabase db = new FakeDatabase();
         ProductService sut = new ProductService(db);
 
-        // Act
         Response response = sut.validateAndAdd(productData);
 
-        // Assert
         String productString = "";
         if (db.product != null) {
             productString = db.product.toString();
         }
         String responseAndProduct = response.toString() + " " + productString;
-        verify(responseAndProduct);
-
+        return responseAndProduct;
     }
 
     class FakeDatabase extends DatabaseAccess {
