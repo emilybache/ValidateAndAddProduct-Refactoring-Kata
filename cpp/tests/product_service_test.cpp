71a7a43eaf18515e4686ca0d13e9944d2126a3b7
emily <emily@bacheconsulting.com>
2020-11-26 08:25:53 +0100

    cpp ready for combinations

--- cpp/tests/product_service_test.cpp
+++ cpp/tests/product_service_test.cpp
@@ -4,8 +4,24 @@
 #include "ApprovalTests.hpp"
 #include "FakeDatabase.h"
 
-TEST_CASE ("test_validate_and_add") {
-    auto productData = new ProductFormData("Sample product", "Lipstick", 5, 10, false);
+static string doValidateAndAdd(const string &name, const string &productType, int weight, int suggestedPrice,
+                               bool packagingRecyclable);
+
+TEST_CASE ("test_validate_and_add")
+{
+    const std::string name = "Sample product";
+    const std::string productType = "Lipstick";
+    int weight = 5;
+    int suggestedPrice = 10;
+    bool packagingRecyclable = false;
+    string responseAndProduct = doValidateAndAdd(name, productType, weight, suggestedPrice, packagingRecyclable);
+
+    Approvals::verify(responseAndProduct);
+}
+
+static string doValidateAndAdd(const string &name, const string &productType, int weight, int suggestedPrice,
+                               bool packagingRecyclable) {
+    auto productData = new ProductFormData(name, productType, weight, suggestedPrice, packagingRecyclable);
     auto db = new FakeDatabase();
     auto sut = new ProductService(db);
 
@@ -17,6 +33,5 @@ TEST_CASE ("test_validate_and_add") {
         responseAndProduct.append(" ");
         responseAndProduct.append(db->product->to_string());
     }
-
-    Approvals::verify(responseAndProduct);
+    return responseAndProduct;
 }
