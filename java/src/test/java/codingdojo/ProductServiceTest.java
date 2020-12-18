3c790f9b2bfb3bf9fff19fd21575b8741569a4f7
Emily Bache <emily@bacheconsulting.com>
2020-12-18 07:11:31 +0000

    go back to pairwise testing to show how it works

--- java/src/test/java/codingdojo/ProductServiceTest.java
+++ java/src/test/java/codingdojo/ProductServiceTest.java
@@ -9,8 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ProductServiceTest {
     @Test
     void validateAndAdd() {
-        CombinationApprovals.verifyAllCombinations(
-        //CombinationApprovals.verifyBestCoveringPairs( // best covering pairs has fewer combinations
+        //CombinationApprovals.verifyAllCombinations(
+        CombinationApprovals.verifyBestCoveringPairs( // best covering pairs has fewer combinations
                 this::doValidateAndAdd,
                 new String [] {"Sample product", "", "Queen"},
                 new String [] {"Lipstick", "", "Eyeshadow", "Foundation", "Mascara", "Blusher", "Unknown"},
